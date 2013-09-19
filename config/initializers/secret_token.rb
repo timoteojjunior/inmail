f4d5b6af38903c258637f1bacd878f2401aced40
Timoteo Junior <timoteojjunior@gmail.com>
2013-09-19 10:47:54 +0200

    Created the App, updated the Readme file and the Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inmail::Application.config.secret_token = 'd58ea6bfe2d573e3a907800cbbfbb2cc0be2bc5d41119e6132d75cc86ced208f0d3f2c1059a73ac3606177f2d8215ef3ccf0738371091366c4ca92f6b4a30c08'
