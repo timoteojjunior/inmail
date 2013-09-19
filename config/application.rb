4b386886bbe5e5888127710b9ab792abba65f6c1
Timoteo Junior <timoteojjunior@gmail.com>
2013-09-19 11:06:21 +0200

    Installed devise and created User Model

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,9 @@ module Inmail
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Devise needs it
+    config.assets.initialize_on_precompile = false
+
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ Inmail::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  #Devise needs it
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 end
